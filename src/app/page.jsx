86fffea585841bc144982f70f28143f6e14040f6
Kaycee47 <146338716+Kaycee47@users.noreply.github.com>
2024-04-26 08:51:03 +0100

    'withdrawal'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Dashboard from "@/component/dashboardBigDaav/page";
+import Withdrawal from "@/component/WithdrawalKaycee/page"
 
 export default function Home() {
   return (
-    <Dashboard/>
+    <Withdrawal/>
+    
+    
+
     
   );
 }
