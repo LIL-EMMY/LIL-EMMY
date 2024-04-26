7be7dd754fba581cc079f4c754056ce264647df3
Kaycee47 <146338716+Kaycee47@users.noreply.github.com>
2024-04-26 08:59:58 +0100

    'withdraw'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+ 
 import React from 'react'
 import Sidenav from '../component/side-nav/page'
 import Navbar from '../component/navbar/page'
-=======
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Dashboard from "@/component/dashboardBigDaav/page";
