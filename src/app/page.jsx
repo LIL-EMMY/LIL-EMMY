e8789e425e30db29e044e4d36a7a06fef41b43f6
anthony david tom <anthodave01@gmail.com>
2024-04-26 09:47:44 +0100

    u

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
+ 
+import React from 'react'
+import Sidenav from '../component/side-nav/page'
+import Navbar from '../component/navbar/page'
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Dashboard from "@/component/dashboardBigDaav/page";
 
 
-export default function Home() {
+export default function page() {
   return (
     <>
       <Dashboard/>
