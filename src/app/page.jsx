1ec03516c7116a6a803a9c9836aa530710cb0dad
anthony david tom <anthodave01@gmail.com>
2024-04-26 09:40:33 +0100

    card completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Dashboard from "@/component/dashboardBigDaav/page";
 
+
 export default function Home() {
   return (
-    <Dashboard/>
+    <>
+      <Dashboard/>
+      
+    </>
     
   );
 }
