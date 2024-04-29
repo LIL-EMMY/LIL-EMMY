40c29424172bd8d996d75c3fd21a6cafffb4c8ec
Johnbosco <emmyokafor637@gmail.com>
2024-04-29 14:42:37 +0100

    update

--- src/component/ThemeMode/ThemeToggle.jsx
+++ src/component/ThemeMode/ThemeToggle.jsx
@@ -6,14 +6,14 @@ import ThemeContext from "./Context";
 import { GoSun } from "react-icons/go";
 import { FaMoon } from "react-icons/fa";
 import { FaSun } from "react-icons/fa6";
-// import styles from "./page.module.css";
+import styles from "./page.module.css";
 import styles from '@/component/side-nav/page.module.css';
 
 const ThemeToggle = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div className="toggle">
+    <div className={styles.toggle}>
       <button className={<GoSun onClick={toggleTheme} />}>
         {theme === "light" ? "Dark" : "Light"}
         <FaMoon onClick={toggleTheme} style={{ backgroundColor: "black" }} />
